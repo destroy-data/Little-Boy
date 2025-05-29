b84bf4541a2e727d7a78ee7a3df54a461ca54b3f
Marcin Banach <marcin.banach14472@gmail.com>
2025-05-29 19:54:11 +0200

    main emulator loop

--- src/core/emulator.cpp
+++ src/core/emulator.cpp
@@ -3,10 +3,14 @@
 
 template<typename Tcartridge, typename Tmemory, typename Tcpu, typename Tppu>
 void Emulator<Tcartridge, Tmemory, Tcpu, Tppu>::tick() {
+    const auto cpuTicks = cpu.tick();
+    for( auto i = 1u; i < cpuTicks; i++ ) {
+        ppu.tick();
+    }
+
     const bool cpuDoubleSpeed = memory.read( addr::key1 ) & ( 1 << 7 );
-    cpu.tick();
-    ppu.tick();
     if( cpuDoubleSpeed )
         cpu.tick();
+
     //apu.tick();
 }
